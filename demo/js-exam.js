70db2f5425a30c4b2516c570caf715a06a15ed14
Krasimir Tsonev <krasimir@outset.ws>
2020-03-07 07:05:19 +0200

    WIP

---
+++ demo/js-exam.js
@@ -0,0 +1,23 @@
+/* A comment */
+// Another comment
+
+const answerA = 42;
+let answerB = "hello world";
+var answerC = "Hey";
+const result = 40 + 100;
+const isItTrue = 100 > 20;
+
+const getAnswerA = function (name) {
+  return `Hey ${name}`;
+}
+function getAnswerB() {
+  return 'Hey';
+}
+class Test extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <p>Hello world</p>;
+  }
+}
