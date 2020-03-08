035d2ba7cba13da1b9c34fac0930651892fd195d
Krasimir Tsonev <krasimir@outset.ws>
2020-03-08 07:25:12 +0200

    Updating colors

--- demo/js.js
+++ demo/js.js
@@ -1,4 +1,10 @@
+import * as Foo from 'react';
+
 const hello = 'I am never used';
+let foo = 'bar';
+let why = 42;
+
+console.log(hello, foo)
 
 doesNotExist.nope(`I'm never imported`);
 
@@ -15,3 +21,28 @@ function wes() {
 const wes = () => { };
 
 console.log(joe, wes);
+
+const assert = require('assert');
+const fs = require('fs');
+
+const tests = [];
+const code = fs.readFileSync('aaa.js');
+
+function it(name, fn) {
+	tests.push({ name, fn });
+}
+function run() {
+	console.log(code);
+	tests.forEach(t => {
+		try {
+			t.fn();
+			console.log('✅', t.name);
+		} catch (e) {
+			console.log('❌', t.name);
+			console.log(e.stack);
+		}
+	})
+}
+
+run();
+
